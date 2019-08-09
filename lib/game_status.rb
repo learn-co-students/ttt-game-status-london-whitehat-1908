59b0733ba4dee0d22ef6e3b7f6229df54d147d71
Oliver <olliepar01@gmail.com>
2019-08-09 16:08:28 +0100

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,42 +4,57 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-def full?(board) 
-  x = 0
-  count = 0
-  while x >= 8 do
-    if board[x] == "X" || board[x] == "O" then
-      count = count + 1
-    end
-    if board[x] == " " then
-      return false
-    end
-  end
-end
+
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
 
 def won?(board)
-  WIN_COMBINATIONS = [
-    [0,1,2], 
-    [3,4,5],
-    [6,7,8],
-    [0,4,8],
-    [2,4,6],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8]
-  ]
-  for board in WIN_COMBINATIONS do
-    win_index_1 = WIN_COMBINATIONS[0]
-    win_index_2 = WIN_COMBINATIONS[1]
-    win_index_3 = WIN_COMBINATIONS[2]
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
-  else
-    false
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2] 
+      position_3 = board[win_index_3]
+      
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") or (position_1 == "O" && position_2 == "O" && position_3 == "O") then
+        return win_combination
+      else
+        false
+      end
   end
-end
+  false
 end
 
-def draw(board) 
-  
+def draw?(board) 
+  !won?(board) && !board.include?(" ")
+end
+
+def full?(board) 
+  board.each { |space|
+  if space == " " then 
+    return false
+  end
+  }
+  return true
+end
+def over?(board)
+  if (won?(board)) || (draw?(board)) then 
+    return true
+  end
+  return false
+end
+
+def winner(board) 
+  if won?(board) then 
+    return board[won?(board)[0]]
+  end
 end
